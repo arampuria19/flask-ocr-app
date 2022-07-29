c9353e387f0449075a19700b84c71a812862a75c
Kaushik Dey <kaushikdey647@gmail.com>
2022-07-29 16:30:05 +0530

    Updated HTML

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from flask import Flask, request,render_template, jsonify
 from app.functions import functions
+import numpy as np
+
 app = Flask(__name__)
 
 '''
@@ -16,13 +18,15 @@ def index():
 
 @app.route('/api',methods=['GET','POST'])
 def api():
-    if request.method == 'GET':
-        pass
     if request.method == 'POST':
-        pass
         # img = request.files.get('img')
         #A function that takes in image
-        #Returns the dict of 
+        #Returns the dict of
+        img_arr = cv2.imdecode(
+            np.fromstring(request.data,np.uint8),
+            cv2.IMREAD_COLOR
+        )
+
 
 if __name__ == '__main__':
     app.run()
